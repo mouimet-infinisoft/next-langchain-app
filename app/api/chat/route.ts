44bbcf4cbf36578cfa6f9848b7f7208c7902426c
Martin Ouimet <mouimet@infinisoft.world>
2023-10-28 23:58:45 -0400

    added UI

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,17 @@
 import { StreamingTextResponse, LangChainStream, Message } from 'ai';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { AIMessage, HumanMessage } from 'langchain/schema';
+import {BufferMemory} from 'langchain/memory'
+import {ConversationChain} from 'langchain/chains'
 
 export const runtime = 'edge';
+// const model = new ChatOpenAI({
+//   streaming: true,
+// });
+// const memory = new BufferMemory();
+// const chain = new ConversationChain({ llm: model, memory: memory });
+
+
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
@@ -12,6 +21,9 @@ export async function POST(req: Request) {
   const llm = new ChatOpenAI({
     streaming: true,
   });
+
+  // const res1 = await chain.call({ input: messages.at(-1).content },{},);
+
 
   llm
     .call(
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,54 @@
 'use client';
 
 import { useChat } from 'ai/react';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Chat() {
+  const [isChatOpen, setChatOpen] = useState(false);
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const messagesEndRef = useRef(null);
 
+  const scrollToBottom = () => {
+    //@ts-ignore
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  useEffect(scrollToBottom, [messages]);
   return (
-    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-      {messages.length > 0
-        ? messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
-        : null}
+    <div>
+      <button
+        className="fixed bottom-4 right-4 p-4 rounded-full bg-blue-500 text-white"
+        onClick={() => setChatOpen(!isChatOpen)}
+      >
+        Chat
+      </button>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+      {isChatOpen && (
+        <div className="fixed bottom-16 right-4 p-4 bg-white rounded shadow-lg flex flex-col" style={{ width: '300px', height: '400px' }}>
+          <div className="bg-gray-300 p-2 font-bold">Chat</div>
+          <div className="overflow-auto flex-grow p-2">
+            {messages.length > 0
+              ? messages.map(m => (
+                <div key={m.id} className={`whitespace-pre-wrap p-2 rounded mb-2 ${m.role === 'user' ? 'bg-blue-200 text-blue-700 text-right ml-auto' : 'bg-gray-200 text-gray-700 mr-auto'}`}>
+                  <div className="font-bold">{m.role === 'user' ? 'User' : 'AI'}</div>
+                  {m.content}
+                </div>
+              ))
+              : null}
+            <div ref={messagesEndRef}></div>
+          </div>
+          <form onSubmit={handleSubmit} className="mt-4 border-t border-gray-300 pt-2">
+            <input
+              className="w-full border border-gray-300 rounded p-2"
+              value={input}
+              placeholder="Say something..."
+              onChange={handleInputChange}
+            />
+          </form>
+        </div>
+      )}
+
     </div>
+
   );
 }
